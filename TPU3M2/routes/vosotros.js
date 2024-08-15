ef1231a7be0151c3d9c6cb6f64b294bfaf5e54aa
ClaudioGTZ <claudioggutierrez@gmail.com>
2024-08-15 09:39:39 -0300

    Delete TPU3M2 directory

--- TPU3M2/routes/vosotros.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* Pagina nosotros */
-router.get('/', function(req, res, next) {
-  res.send('Pagina Vosotros');
-});
-
-module.exports = router;
