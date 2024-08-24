63273d618f28db3a7b0731897994249241415e0c
Claudio GTZ <claudioggutierrez@gmail.com>
2024-08-23 23:01:22 -0300

    Sitio Vero Primer versión

---
+++ sitio_vero/routes/index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+router.post('/', async (req, res, next) => {
+  var nombre = req.body.nombre;
+  var mail = req.body.mail;
+  var mensaje = req.body.mensaje;
+
+  var obj = {
+    to: 'claudioggutierrez@gmail.com',
+    subject: 'Consulta sobre curso',
+    html: nombre + " " + "envió una consulta: " + mensaje + "Responder a " + mail,
+  }
+
+  var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    }
+  })
+
+  var info = await transporter.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente',
+  });
+
+})
+
+module.exports = router;
