5c93ceeeab63954385b04854d2bda941511e8c7d
Claudio GTZ <claudioggutierrez@gmail.com>
2024-10-08 17:55:37 -0300

    Actualización MA U4

---
+++ sitio_vero/routes/admin/login.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+
+var usuariosModel = require('./../../models/usuariosModel');
+
+/* va a buscar la nueva configuración de header y footer */
+router.get('/', function(req, res, next) {
+  res.render('admin/login', {
+    layout:'admin/layout'
+    });
+  });
+
+/* elimina los datos de inicio de sesión */
+router.get('/logout', function (req, res, next) {
+  req.session.destroy();
+  res.render('admin/login', {
+    layout: 'admin/layout'
+  });
+});
+
+  router.post('/', async (req, res, next) => {
+    try {
+        var usuario = req.body.usuario;
+        var password = req.body.password;
+        // console.log(req.body);
+
+        var data = await usuariosModel.getUserAndPassword(usuario, password);
+
+        if (data != undefined) {
+          req.session.id_usuario = data.id_user;
+          req.session.nombre = data.usuario;
+          res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/login', {
+            layout: 'admin/layout',
+            error: true
+          });
+        } //fin else
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports = router;
